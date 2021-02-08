66cdda6b747453f510478d01888e34fb79cfb249
Dyana Monroy <dyana.monroy17@live.com>
2021-02-08 14:46:25 -0500

    made a new react project

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+// everything in React is JavaScript!
 ReactDOM.render(
   <h1>Hello world</h1>,
   document.getElementById('root')
