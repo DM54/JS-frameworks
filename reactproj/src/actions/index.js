4871c8aa1ed22cefbd04d62e62ee953244d890c0
Dyana Monroy <dyana.monroy17@live.com>
2021-02-16 20:25:15 -0500

    coded some more

--- reactproj/src/actions/index.js
+++ reactproj/src/actions/index.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 export function getResources() {
 
-  return axios.get('http://localhost:3000/api/resources')
+  return axios
+  .get('http://localhost:3000/api/resources')
   .then(res => res.data)
 
+}
+
+export function UpdateResourcesApi({reourceID, ressourceData})
+{
+  return axios
+  .patch(`'http://localhost:3000/api/resources/${reourceID}'`, ressourceData)
+  .then(res => res.data);
 }
--- reactproj/src/components/ResourceUpdate.js
+++ reactproj/src/components/ResourceUpdate.js
@@ -1,54 +1,18 @@
 import React from 'react';
+import ResourceForm from "./ResourceForm";
+import {UpdateResourcesApi} from '../actions';
+
 
 const ResourceUpdate = ({resource}) => {
 
-  return (
-     <>
+  const UpdateResources = (formData) =>{
 
-          <form>
-            <div className="mb-3">
-              <label htmlFor="title">Title</label>
-              <input
-              value = {resource.title}
-              type="text"
-              className="form-control"
-              id="title"
-              placeholder="How to survice in mountains" />
-            </div>
-            <div className="mb-3">
-              <label for="description">Description</label>
-              <textarea
-               value = {resource.description}
-              className="form-control"
-              id="description"
-              placeholder="Just some description"></textarea>
-            </div>
 
-            <div className="mb-3">
-              <label htmlFor="link">Resource Link</label>
-              <div className="input-group">
-                <input
-                 value = {resource.link}
-                type="text"
-                className="form-control"
-                 id="link"
-                 placeholder="Username" />
-              </div>
-            <div className="mb-3">
-              <label htmlFor="type">Type</label>
-              <input
-              type="text"
-              className="form-control"
-              id="type"
-              placeholder="Very interesting book" />
-            </div>
-
-            </div>
-            <hr className="mb-4" />
-            <button className="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
-          </form>
-        </>
+  }
+  return(
+ <ResourceForm onSubmit={UpdateResources} resource={resource}></ResourceForm>
   )
+
 }
 
 export default ResourceUpdate;
